b435a164610c015de125b775671ffd1650c1aa1e
Arielle Moore <acm.young@gmail.com>
2022-01-26 15:26:06 -0300

    chore: update paths and add file exts to .gitignore.

--- console_app_demo/Program.cs
+++ console_app_demo/Program.cs
@@ -14,14 +14,14 @@ catch (System.IO.FileNotFoundException)
     Console.WriteLine("XML file import failed, sorry!");
 }
 
-string jsonFile = "C:/Users/acm/source/repos/console_app_demo/console_app_demo/files/RightslineSample.json";
+string jsonFile = "C:/Users/acm/source/repos/console_app_demo_simple/console_app_demo/files/RightslineSample.json";
 string jsonFormattedText = JsonConvert.SerializeXmlNode(importXmlDoc, Newtonsoft.Json.Formatting.Indented);
 File.WriteAllText(jsonFile, jsonFormattedText);
 
 Console.WriteLine(File.ReadAllText(jsonFile));
 
 string extractedTextFromJson = System.IO.File.ReadAllText(jsonFile);
-string xmlFile = "C:/Users/acm/source/repos/console_app_demo/console_app_demo/files/RightslineSample2.xml";
+string xmlFile = "C:/Users/acm/source/repos/console_app_demo_simple/console_app_demo/files/RightslineSample2.xml";
 XNode finalXmlText = JsonConvert.DeserializeXNode(extractedTextFromJson, "Root");
 File.WriteAllText(xmlFile, finalXmlText.ToString());
 
