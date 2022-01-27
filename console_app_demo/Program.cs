abb66afba4aaf879dec99dc758af6b88e04b7596
Arielle Moore <acm.young@gmail.com>
2022-01-27 11:55:20 -0300

    refactor: apply structure and remove implicit using

--- console_app_demo/Program.cs
+++ console_app_demo/Program.cs
@@ -2,29 +2,37 @@
 using System.Xml;
 using System.Xml.Linq;
 
-Console.WriteLine("Please input the absolute path to your XML file so I can automagically convert it:");
-string filePath = @"" + Console.ReadLine();
+namespace ConsoleAppDemo;
 
-XmlDocument importXmlDoc = new XmlDocument();
-try
+public class Program
 {
-    importXmlDoc.Load(filePath);
-    Console.WriteLine("I found your XML file! Beep boop...");
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Please input the absolute path to your XML file so I can automagically convert it:");
+        string? filePath = Console.ReadLine();
+
+        XmlDocument importXmlDoc = new();
+        try
+        {
+            importXmlDoc.Load(filePath);
+            Console.WriteLine("I found your XML file! Beep boop...");
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("XML file not found at this location, bummer!");
+        }
+
+        string baseDir = AppDomain.CurrentDomain.BaseDirectory;
+        string jsonFilePath = Path.Combine(baseDir, $@"..\..\..\..\console_app_demo\files\XmlToJsonResult.json");
+        string jsonFormattedText = JsonConvert.SerializeXmlNode(importXmlDoc, Newtonsoft.Json.Formatting.Indented);
+        File.WriteAllText(jsonFilePath, jsonFormattedText);
+
+        string textExtractedFromJson = File.ReadAllText(jsonFilePath);
+        string xmlFilePath = Path.Combine(baseDir, $@"..\..\..\..\console_app_demo\files\JsonToXmlResult.xml");
+        XNode xmlDeserializedText = JsonConvert.DeserializeXNode(textExtractedFromJson, "Root");
+        File.WriteAllText(xmlFilePath, xmlDeserializedText.ToString());
+
+        Console.WriteLine("Conversion process is complete! Please navigate to the files folder to view your new JSON and XML files.");
+        Console.WriteLine("Have a lovely day!");
+    }
 }
-catch (System.IO.FileNotFoundException)
-{
-    Console.WriteLine("XML file not found at this location, bummer!");
-}
-
-string baseDir = AppDomain.CurrentDomain.BaseDirectory;
-string jsonFilePath = System.IO.Path.Combine(baseDir, $@"..\..\..\..\console_app_demo\files\RightslineSampleFromXml.json");
-string jsonFormattedText = JsonConvert.SerializeXmlNode(importXmlDoc, Newtonsoft.Json.Formatting.Indented);
-File.WriteAllText(jsonFilePath, jsonFormattedText);
-
-string textExtractedFromJson = System.IO.File.ReadAllText(jsonFilePath);
-string xmlFilePath = System.IO.Path.Combine(baseDir, $@"..\..\..\..\console_app_demo\files\RightslineSampleFromJson.xml");
-XNode xmlDeserializedText = JsonConvert.DeserializeXNode(textExtractedFromJson, "Root");
-File.WriteAllText(xmlFilePath, xmlDeserializedText.ToString());
-
-Console.WriteLine("Conversion process is complete! Please navigate to the files folder to view your new JSON and XML files.");
-Console.WriteLine("Have a lovely day!");
