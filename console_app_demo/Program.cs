3e905d860bc1185e0cc93619bd20f6410cb5f0b5
Arielle Moore <acm.young@gmail.com>
2022-02-01 12:32:29 -0300

    refactor: small restructure

--- console_app_demo/Program.cs
+++ console_app_demo/Program.cs
@@ -12,16 +12,28 @@ public class Program
         string? filePath = Console.ReadLine();
 
         XmlDocument importXmlDoc = new();
+
         try
         {
             importXmlDoc.Load(filePath);
             Console.WriteLine("I found your XML file! Beep boop...");
         }
-        catch (FileNotFoundException)
+        catch (Exception exception)
         {
-            Console.WriteLine("XML file not found at this location, bummer!");
+            if (exception is FileNotFoundException || exception is XmlException || Path.GetExtension(filePath) != ".xml")
+            {
+                Console.Clear();
+                Console.WriteLine("XML file is either corrupted or not found at this location, bummer!");
+                Environment.Exit(1);
+            }
         }
 
+        XmlToJsonToXmlConversion(importXmlDoc);
+        SuccessMessage();
+    }
+
+    private static void XmlToJsonToXmlConversion(XmlNode importXmlDoc)
+    {
         string baseDir = AppDomain.CurrentDomain.BaseDirectory;
         string jsonFilePath = Path.Combine(baseDir, $@"..\..\..\..\console_app_demo\files\XmlToJsonResult.json");
         string jsonFormattedText = JsonConvert.SerializeXmlNode(importXmlDoc, Newtonsoft.Json.Formatting.Indented);
@@ -30,9 +42,18 @@ public class Program
         string textExtractedFromJson = File.ReadAllText(jsonFilePath);
         string xmlFilePath = Path.Combine(baseDir, $@"..\..\..\..\console_app_demo\files\JsonToXmlResult.xml");
         XNode xmlDeserializedText = JsonConvert.DeserializeXNode(textExtractedFromJson, "Root");
-        File.WriteAllText(xmlFilePath, xmlDeserializedText.ToString());
+        if (xmlDeserializedText is not null)
+        {
+            File.WriteAllText(xmlFilePath, xmlDeserializedText.ToString());
+        }
+    }
 
+    private static string SuccessMessage()
+    {
+        Console.Clear();
         Console.WriteLine("Conversion process is complete! Please navigate to the files folder to view your new JSON and XML files.");
+        Thread.Sleep(1000);
         Console.WriteLine("Have a lovely day!");
+        return string.Empty;
     }
 }
